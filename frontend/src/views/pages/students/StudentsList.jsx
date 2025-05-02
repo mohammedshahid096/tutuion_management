8c3d7def87f51fcf5eb36d2bec923acebeb6c6e4
Mohammed shahid <www.mohdshahid303@gmail.com>
2025-05-02 16:03:37 +0530

    added skeleton to the students lists

--- frontend/src/views/pages/students/StudentsList.jsx
+++ frontend/src/views/pages/students/StudentsList.jsx
@@ -4,10 +4,39 @@ import MainWrapper from '@/views/layouts/Mainwrapper';
 import { useDispatch, useSelector } from 'react-redux';
 import { batchActions, boardActions, studentActions } from '@/redux/combineActions';
 import _ from 'lodash';
+import { Skeleton } from '@/components/ui/skeleton';
 
 const breadCrumbs = [{ label: 'students', href: null }];
 
 const classRooms = Array.from({ length: 12 }, (_, i) => i + 1); // 1 to 12
+
+const DataTableSkeleton = memo(({ numRows = 6, numCols = 5 }) => {
+  return (
+    <div className="rounded-md border overflow-hidden">
+      {/* Table Header */}
+      <div className="flex items-center h-12 px-4 bg-muted text-sm font-medium">
+        {Array.from({ length: numCols }).map((_, i) => (
+          <div key={i} className="w-full min-w-[100px]">
+            <Skeleton className="h-5 w-3/4 rounded" />
+          </div>
+        ))}
+      </div>
+
+      {/* Table Body */}
+      <div className="divide-y">
+        {Array.from({ length: numRows }).map((_, rowIndex) => (
+          <div key={rowIndex} className="flex items-center h-16 px-4 text-sm gap-6">
+            {Array.from({ length: numCols }).map((_, colIndex) => (
+              <div key={colIndex} className="w-full min-w-[100px] py-2">
+                <Skeleton className="h-4 w-full rounded" />
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+});
 
 const StudentsList = () => {
   const { getBatchesListAction } = batchActions;
@@ -132,15 +161,19 @@ const StudentsList = () => {
 
   return (
     <MainWrapper breadCrumbs={breadCrumbs}>
-      <StudentsListComponent
-        classRooms={classRooms}
-        boardTypes={boardsList}
-        batchTypes={batchesList}
-        data={studentsList}
-        info={info}
-        setInfo={setInfo}
-        filterChangeHandlerFunction={filterChangeHandlerFunction}
-      />
+      {info?.loading ? (
+        <DataTableSkeleton />
+      ) : (
+        <StudentsListComponent
+          classRooms={classRooms}
+          boardTypes={boardsList}
+          batchTypes={batchesList}
+          data={studentsList}
+          info={info}
+          setInfo={setInfo}
+          filterChangeHandlerFunction={filterChangeHandlerFunction}
+        />
+      )}
     </MainWrapper>
   );
 };
