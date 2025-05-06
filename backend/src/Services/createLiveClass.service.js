e202fe33e50fd25b02afb7f4efbf7ac3bc24a5a1
Mohammed shahid <www.mohdshahid303@gmail.com>
2025-05-06 17:16:24 +0530

    cron-job is added and added service

---
+++ backend/src/Services/createLiveClass.service.js
@@ -0,0 +1,113 @@
+const moment = require("moment");
+const attendanceModel = require("../Schema/attendance/attendance.model");
+const GoogleCalendarServiceClass = require("./google.calendar.service");
+const NodeMailerServiceClass = require("../aws_ses/mails/mail.index");
+
+class LiveClassServiceClass {
+  constructor(
+    studentId,
+    studentDetails,
+    startTime,
+    endTime,
+    summary,
+    description,
+    batchId,
+    userId
+  ) {
+    this.studentId = studentId;
+    this.studentDetails = studentDetails;
+    this.currentDay = moment().format("dddd").toLowerCase();
+    this.currentDate = moment().format("LLL");
+    this.summary = summary || null;
+    this.description = description || null;
+    this.studentDetails = null;
+    this.startDate = moment(startTime, "HH:mm");
+    this.endDate = moment(endTime, "HH:mm");
+    this.googleMeet = null;
+    this.batchId = batchId;
+    this.userId = userId;
+  }
+
+  async checkAvailabilityMethod() {
+    const existingRecord = await attendanceModel.findOne({
+      student: this.studentId,
+      startDate: this.startDate,
+    });
+    return existingRecord;
+  }
+
+  async createGoogleMeetMethod(studentDetails = {}) {
+    const googleCalendarService = await new GoogleCalendarServiceClass(
+      this.studentId
+    );
+    let googleEventDetails = {
+      summary: this.summary,
+      description: this.description,
+      startDate: this.startDate,
+      endDate: this.endDate,
+      timezone: "Asia/Kolkata",
+      location: "Google Meet" || "",
+      attendees: [{ email: studentDetails?.email }],
+    };
+
+    await googleCalendarService.initialize();
+    let eventResponseDetails = await googleCalendarService.createEvent(
+      "primary",
+      googleEventDetails
+    );
+
+    if (eventResponseDetails) {
+      this.googleMeet = {
+        meetLink: eventResponseDetails.hangoutLink,
+        conferenceId: eventResponseDetails.conferenceData.conferenceId,
+        details: eventResponseDetails,
+      };
+    }
+
+    return this.googleMeet;
+  }
+
+  async createMeetDocument() {
+    let details = {
+      student: this.studentId,
+      summary: this.summary,
+      description: this.summary,
+      startDate: this.startDate,
+      endDate: this.endDate,
+      day: this.currentDay,
+      class: this.studentDetails?.class,
+      board: this.studentDetails?.boardType,
+      batch: this.batchId,
+      createdBy: req.user._id,
+      updatedBy: req.user._id,
+      googleMeet: this.googleMeet,
+    };
+
+    const data = new attendanceModel(details);
+    await data.save();
+  }
+
+  async sendMailMethod() {
+    let mailDetails = {
+      student_name: this.studentDetails?.name,
+      session_summary: this.summary,
+      session_description: this.description,
+      session_date: moment(this.startDate).format("LLL"),
+      session_time:
+        moment(this.startDate).format("hh:mm A") +
+        " - " +
+        moment(this.endDate).format("hh:mm A"),
+      session_meet_link: this.googleMeet.meetLink,
+    };
+
+    const nodeMailerService = new NodeMailerServiceClass();
+    await nodeMailerService.sendMail(
+      studentDetails.email,
+      "liveSessionReminderTemplate",
+      null,
+      mailDetails
+    );
+  }
+}
+
+module.exports = LiveClassServiceClass;
