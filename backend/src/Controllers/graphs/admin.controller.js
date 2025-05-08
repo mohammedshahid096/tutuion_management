98853ac5d84715787f44b4845262b15b7139dbae
Mohammed shahid <www.mohdshahid303@gmail.com>
2025-05-08 15:18:00 +0530

    added classwise students and boardWise

--- backend/src/Controllers/graphs/admin.controller.js
+++ backend/src/Controllers/graphs/admin.controller.js
@@ -4,6 +4,7 @@ const userModel = require("../../Schema/users/user.model");
 const { ADMIN, STUDENT } = require("../../Constants/roles.constants");
 const errorHandling = require("../../Utils/errorHandling");
 const moment = require("moment");
+const { boards } = require("../../Constants/model.constants");
 
 const adminDashboardGraphController = async (req, res, next) => {
   try {
@@ -15,8 +16,38 @@ const adminDashboardGraphController = async (req, res, next) => {
       { $group: { _id: "$gender", count: { $sum: 1 } } },
       { $project: { _id: 0, gender: "$_id", count: 1 } },
     ];
+    const classWiseStudentsGraphAggregation = [
+      { $match: { role: STUDENT } },
+      { $group: { _id: "$class", count: { $sum: 1 } } },
+      { $project: { _id: 0, classRoom: "$_id", count: 1 } },
+    ];
+    const boardWiseStudentsGraphAggregation = [
+      { $match: { role: STUDENT } },
+      { $group: { _id: "$boardType", count: { $sum: 1 } } },
+      {
+        $lookup: {
+          from: boards,
+          localField: "_id",
+          foreignField: "_id",
+          as: "board",
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          boardName: { $arrayElemAt: ["$board.name", 0] },
+          count: 1,
+        },
+      },
+    ];
 
     let genderGraphData = await userModel.aggregate(genderGraphAggregation);
+    let classWiseStudentsGraphData = await userModel.aggregate(
+      classWiseStudentsGraphAggregation
+    );
+    let boardWiseStudentsGraphData = await userModel.aggregate(
+      boardWiseStudentsGraphAggregation
+    );
     logger.info(
       "Controller - graph.controller - AdminDashboardGraphController - Start"
     );
@@ -24,7 +55,11 @@ const adminDashboardGraphController = async (req, res, next) => {
     res.status(200).send({
       success: true,
       statusCode: 200,
-      data: { genderGraphData },
+      data: {
+        genderGraphData,
+        classWiseStudentsGraphData,
+        boardWiseStudentsGraphData,
+      },
     });
   } catch (error) {
     logger.error("Controller-user.controller-LoginUserController-Error", error);
