b60cca524a5f4fcdbf617bbdac35fa1c61434bd2
Mohammed shahid <www.mohdshahid303@gmail.com>
2025-05-09 12:43:52 +0530

    added docker files and compose file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.xlsx"],
+  assetsInclude: ['**/*.xlsx'],
   css: {
     postcss: {
       plugins: [tailwindcss()],
@@ -13,13 +13,17 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 
   build: {
     sourcemap: true,
-    publicDir: "./src/assets",
-    assetsDir: "./src/assets",
+    publicDir: './src/assets',
+    assetsDir: './src/assets',
+  },
+  server: {
+    host: '0.0.0.0', // This is crucial for Docker
+    port: 5173,
   },
 });
