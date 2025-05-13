6beda6860220d934d7b65fe1672994d480005e0a
Mohammed shahid <www.mohdshahid303@gmail.com>
2025-05-13 15:42:50 +0530

    list shown in the admin dashboard

--- frontend/src/redux/combineActions.js
+++ frontend/src/redux/combineActions.js
@@ -5,6 +5,7 @@ import BoardActions from './boards/action';
 import SubjectActions from './subjects/action';
 import StudentActions from './students/action';
 import GraphsActions from './graphs/action';
+import ContactFormActions from './contact/action';
 
 export const loginActions = { ...LoginActions };
 export const userActions = { ...UserActions };
@@ -13,5 +14,6 @@ export const boardActions = { ...BoardActions };
 export const subjectActions = { ...SubjectActions };
 export const studentActions = { ...StudentActions };
 export const graphActions = { ...GraphsActions };
+export const contactActions = { ...ContactFormActions };
 
 // after adding reset the action in to the logout hook
--- frontend/src/views/pages/contact/ContactList.jsx
+++ frontend/src/views/pages/contact/ContactList.jsx
@@ -1,8 +1,100 @@
+import React, { useState, useEffect, useCallback, memo } from 'react';
 import MainWrapper from '@/views/layouts/Mainwrapper';
-import React, { memo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { contactActions } from '@/redux/combineActions';
+import MetaData from '@/utils/MetaData';
+import CustomTable1 from '@/views/components/tables/TableV1';
+import { Badge } from '@/components/ui/badge';
+import moment from 'moment';
+
+const breadCrumbs = [{ label: 'Contact Responses', href: null }];
+
+const headers = [
+  { title: 'Name', key: 'name' },
+  { title: 'email', key: 'email' },
+  { title: 'Phone', key: 'phone' },
+  { title: 'Class', key: 'class' },
+  { title: 'Message', key: 'message' },
+  { title: 'Preferred Time', key: 'preferredTime' },
+  { title: 'Heard From', key: 'heardAboutUs' },
+  { title: 'Heard From', key: 'heardAboutUs' },
+  { title: 'Date', key: 'date' },
+];
+
+const badgeStyles = {
+  morning: 'bg-blue-100 text-blue-800 hover:bg-blue-200 dark:bg-blue-900 dark:text-blue-100',
+  afternoon:
+    'bg-yellow-100 text-yellow-800 hover:bg-yellow-200 dark:bg-yellow-900 dark:text-yellow-100',
+  evening:
+    'bg-purple-100 text-purple-800 hover:bg-purple-200 dark:bg-purple-900 dark:text-purple-100',
+  any: 'bg-gray-100 text-gray-800 hover:bg-gray-200 dark:bg-gray-800 dark:text-gray-100',
+  friend: 'bg-green-100 text-green-800 hover:bg-green-200 dark:bg-green-900 dark:text-green-100',
+  'social media': 'bg-blue-100 text-blue-800 hover:bg-blue-200 dark:bg-blue-900 dark:text-blue-100',
+  newspaper:
+    'bg-yellow-100 text-yellow-800 hover:bg-yellow-200 dark:bg-yellow-900 dark:text-yellow-100',
+  flyer:
+    'bg-purple-100 text-purple-800 hover:bg-purple-200 dark:bg-purple-900 dark:text-purple-100',
+  website:
+    'bg-indigo-100 text-indigo-800 hover:bg-indigo-200 dark:bg-indigo-900 dark:text-indigo-100',
+  other: 'bg-gray-100 text-gray-800 hover:bg-gray-200 dark:bg-gray-800 dark:text-gray-100',
+  default: 'bg-gray-100 text-gray-800',
+};
 
 const ContactList = () => {
-  return <MainWrapper>ContactList</MainWrapper>;
+  const { getContactFormListAction } = contactActions;
+  const dispatch = useDispatch();
+  const { contactList, loading } = useSelector((state) => state.contactFormState);
+
+  const [info, setInfo] = useState({
+    loading: true,
+    limit: 20,
+    currentPage: 1,
+  });
+
+  useEffect(() => {
+    if (!contactList) {
+      fetchFormResponseListHandler();
+    }
+  }, []);
+
+  const fetchFormResponseListHandler = useCallback(
+    async (queryObject) => {
+      let query = {
+        page: queryObject?.currentPage ?? info?.currentPage,
+        limit: queryObject?.limit ?? info?.limit,
+      };
+      dispatch(getContactFormListAction(query));
+    },
+    [contactList, info?.currentPage, info?.limit]
+  );
+  return (
+    <MainWrapper breadCrumbs={breadCrumbs}>
+      <MetaData title="Form Responses | EduExcellence" />
+
+      <CustomTable1
+        headers={headers}
+        docs={contactList?.docs?.map((singleData) => ({
+          ...singleData,
+          preferredTime: (
+            <Badge className={badgeStyles[singleData?.preferredTime || 'default']}>
+              {singleData?.preferredTime}
+            </Badge>
+          ),
+          heardAboutUs: (
+            <Badge className={badgeStyles[singleData?.heardAboutUs || 'default']}>
+              {singleData?.heardAboutUs}
+            </Badge>
+          ),
+          date: moment(singleData?.createdAt).format('LL'),
+        }))}
+        cardTitle="Form Response Data"
+        loading={loading}
+        totalPages={contactList?.totalPages}
+        currentPage={contactList?.currentPage}
+        onPageChange={(page) => console.log(first)(page)}
+      />
+    </MainWrapper>
+  );
 };
 
 export default memo(ContactList);
