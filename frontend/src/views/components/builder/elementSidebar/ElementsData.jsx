5d2e733315c569badfc0f46874c8b678f74bf267
Mohammed shahid <www.mohdshahid303@gmail.com>
2025-05-25 00:53:09 +0530

    elements commented

--- frontend/src/views/components/builder/elementSidebar/ElementsData.jsx
+++ frontend/src/views/components/builder/elementSidebar/ElementsData.jsx
@@ -76,98 +76,98 @@ export default [
     style: {},
     outerStyle: {},
   },
-  {
-    icon: Frame,
-    type: 'logo',
-    label: 'Logo',
-    imageUrl: '/logo.svg',
-    alt: 'logo',
-    url: '#',
-    style: {
-      backgroundColor: '#ffffff',
-      padding: '10px',
-      height: '30%',
-      width: '30%',
-    },
-    outerStyle: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#fff',
-      width: '100%',
-    },
-  },
-  {
-    icon: PanelTop,
-    type: 'LogoHeader',
-    label: 'Logo Header',
-    imageUrl: '/logo.svg',
-    alt: 'logo',
-    url: '#',
-    style: {
-      backgroundColor: '#ffffff',
-      padding: '10px',
-      height: '40%',
-      width: '40%',
-    },
-    outerStyle: {
-      display: 'flex',
-      justifyContent: 'left',
-      alignItems: 'center',
-      backgroundColor: '#fff',
-      width: '100%',
-    },
-  },
-  {
-    icon: SquareSplitVertical,
-    type: 'Divider',
-    label: 'Divider',
-    content: '',
-    style: {
-      color: '#000000',
-      padding: '10px',
-      width: '100%',
-    },
-  },
-  {
-    type: 'SocialIcons',
-    icon: Twitter,
-    label: 'Social Icons',
-    socialIcons: [
-      {
-        icon: 'https://cdn-icons-png.flaticon.com/128/2111/2111463.png',
-        url: '',
-      },
-      {
-        icon: 'https://cdn-icons-png.flaticon.com/128/5968/5968852.png',
-        url: '',
-      },
-      {
-        icon: 'https://cdn-icons-png.flaticon.com/128/5968/5968756.png',
-        url: '',
-      },
-    ],
-    options: [
-      {
-        icon: 'https://cdn-icons-png.flaticon.com/128/2111/2111463.png',
-        url: '',
-      },
-      {
-        icon: 'https://cdn-icons-png.flaticon.com/128/5968/5968852.png',
-        url: '',
-      },
-      {
-        icon: 'https://cdn-icons-png.flaticon.com/128/5968/5968756.png',
-        url: '',
-      },
-    ],
-    style: {
-      width: 40,
-      height: 40,
-    },
-    outerStyle: {
-      display: 'flex',
-      gap: 15,
-    },
-  },
+  // {
+  //   icon: Frame,
+  //   type: 'logo',
+  //   label: 'Logo',
+  //   imageUrl: '/logo.svg',
+  //   alt: 'logo',
+  //   url: '#',
+  //   style: {
+  //     backgroundColor: '#ffffff',
+  //     padding: '10px',
+  //     height: '30%',
+  //     width: '30%',
+  //   },
+  //   outerStyle: {
+  //     display: 'flex',
+  //     justifyContent: 'center',
+  //     alignItems: 'center',
+  //     backgroundColor: '#fff',
+  //     width: '100%',
+  //   },
+  // },
+  // {
+  //   icon: PanelTop,
+  //   type: 'LogoHeader',
+  //   label: 'Logo Header',
+  //   imageUrl: '/logo.svg',
+  //   alt: 'logo',
+  //   url: '#',
+  //   style: {
+  //     backgroundColor: '#ffffff',
+  //     padding: '10px',
+  //     height: '40%',
+  //     width: '40%',
+  //   },
+  //   outerStyle: {
+  //     display: 'flex',
+  //     justifyContent: 'left',
+  //     alignItems: 'center',
+  //     backgroundColor: '#fff',
+  //     width: '100%',
+  //   },
+  // },
+  // {
+  //   icon: SquareSplitVertical,
+  //   type: 'Divider',
+  //   label: 'Divider',
+  //   content: '',
+  //   style: {
+  //     color: '#000000',
+  //     padding: '10px',
+  //     width: '100%',
+  //   },
+  // },
+  // {
+  //   type: 'SocialIcons',
+  //   icon: Twitter,
+  //   label: 'Social Icons',
+  //   socialIcons: [
+  //     {
+  //       icon: 'https://cdn-icons-png.flaticon.com/128/2111/2111463.png',
+  //       url: '',
+  //     },
+  //     {
+  //       icon: 'https://cdn-icons-png.flaticon.com/128/5968/5968852.png',
+  //       url: '',
+  //     },
+  //     {
+  //       icon: 'https://cdn-icons-png.flaticon.com/128/5968/5968756.png',
+  //       url: '',
+  //     },
+  //   ],
+  //   options: [
+  //     {
+  //       icon: 'https://cdn-icons-png.flaticon.com/128/2111/2111463.png',
+  //       url: '',
+  //     },
+  //     {
+  //       icon: 'https://cdn-icons-png.flaticon.com/128/5968/5968852.png',
+  //       url: '',
+  //     },
+  //     {
+  //       icon: 'https://cdn-icons-png.flaticon.com/128/5968/5968756.png',
+  //       url: '',
+  //     },
+  //   ],
+  //   style: {
+  //     width: 40,
+  //     height: 40,
+  //   },
+  //   outerStyle: {
+  //     display: 'flex',
+  //     gap: 15,
+  //   },
+  // },
 ];
