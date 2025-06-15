eb88201f2741580af25551f6f3cc0f2a0f0f1547
Mohammed shahid <www.mohdshahid303@gmail.com>
2025-06-15 20:04:48 +0530

    added render api server url

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -2,7 +2,8 @@ const config_urls = {
   local: 'http://localhost:3000',
   development: 'http://localhost:8000',
   // production: import.meta.env.VITE_SERVER_URL_API,
-  production: 'https://tutuion-management-backend.vercel.app',
+  production2: 'https://tutuion-management-backend.vercel.app',
+  production: 'https://tutuion-management.onrender.com',
 };
 
 export const BASE_URL = config_urls[import.meta.env.VITE_DEVELOPMENT_MODE || 'development'];
