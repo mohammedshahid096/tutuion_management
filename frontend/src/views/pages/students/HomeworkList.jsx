07c2ad76aa63c4ab32ce807d50d07d14e06462b5
Mohammed shahid <www.mohdshahid303@gmail.com>
2025-06-23 17:22:51 +0530

    homework list is rendered

--- frontend/src/views/pages/students/HomeworkList.jsx
+++ frontend/src/views/pages/students/HomeworkList.jsx
@@ -1,31 +1,84 @@
 import React, { useEffect, useCallback, memo, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { batchActions, boardActions, studentActions } from '@/redux/combineActions';
-import StudentRegistrationForm from '@/views/features/students/RegisterComponent';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import toast from 'react-hot-toast';
+import { studentActions } from '@/redux/combineActions';
 import moment from 'moment';
-import { Skeleton } from '@/components/ui/skeleton';
-import { Card, CardHeader, CardContent } from '@/components/ui/card';
 import _ from 'lodash';
+import { format } from 'timeago.js';
 import { useParams } from 'react-router-dom';
 import MetaData from '@/utils/MetaData';
+import CustomTable1 from '@/views/components/tables/TableV1';
+
+const headers = [
+  { title: 'Title', key: 'title' },
+  { title: 'title', key: 'description' },
+  { title: 'Deadline', key: 'deadline' },
+  { title: 'Date', key: 'date' },
+  { title: 'Time Ago', key: 'timeAgo' },
+];
 
 const HomeworkList = () => {
-  const {} = studentActions;
+  const { getStudentHomeworkListAction } = studentActions;
 
   const dispatch = useDispatch();
   const { studentId } = useParams();
-  const {} = useSelector((state) => state.studentState);
+  const { homeworkList, loading } = useSelector((state) => state.studentState);
 
-  const [info, setInfo] = useState({});
+  const [info, setInfo] = useState({
+    limit: 20,
+    currentPage: 1,
+  });
 
-  const fetchStudentDetailHandler = useCallback(async () => {}, [studentId]);
+  useEffect(() => {
+    if (!homeworkList && homeworkList?._id !== studentId) {
+      fetchHomeworkListHandler();
+    }
+  }, []);
+
+  const fetchHomeworkListHandler = useCallback(
+    async (queryObject = {}) => {
+      let query = {
+        page: queryObject?.currentPage ?? info?.currentPage,
+        limit: queryObject?.limit ?? info?.limit,
+      };
+      dispatch(getStudentHomeworkListAction(query));
+    },
+    [studentId, homeworkList, info?.currentPage, info?.limit]
+  );
+
+  const paginationFunctionHandler = useCallback(
+    (page) => {
+      let queryObject = {
+        currentPage: page,
+      };
+
+      setInfo((prev) => ({
+        ...prev,
+        ...queryObject,
+      }));
+      fetchHomeworkListHandler(queryObject);
+    },
+    [info?.currentPage]
+  );
 
   return (
     <div>
       <MetaData title={`Admin Student Homework | EduExcellence `} />
+
+      <CustomTable1
+        headers={headers}
+        docs={homeworkList?.docs?.map((singleData) => ({
+          ...singleData,
+          deadline: moment(singleData?.deadline).format('LLL'),
+          date: moment(singleData?.createdAt).format('L'),
+          timeAgo: format(singleData?.createdAt),
+        }))}
+        cardTitle="Homework List"
+        loading={loading}
+        totalPages={homeworkList?.totalPages}
+        currentPage={homeworkList?.currentPage}
+        onPageChange={paginationFunctionHandler}
+        limit={info?.limit}
+      />
     </div>
   );
 };
